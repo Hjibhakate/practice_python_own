232c6b4174b439a8ef84ba171726f95fcb83c5d9
Hjibhakate <hjibhakte2004@gmail.com>
2026-05-31 23:36:02 +0530

    print a table

--- full_python.py
+++ full_python.py
@@ -1,58 +1,8 @@
-students = []
-attendances = {}
+def print_table(number):
 
-while True :
+    for i in range (1,11):
+        print( number," * ", i ,"=" , number * i ) 
 
-    print("\n === MINI ATTENDEANCE SYSTEM ====")
+num = int(input("Enter the number you want to print : "))
 
-    print("1.Add Student ")
-    print("2. Marks Attendance")
-    print("3.Show Attendance ")
-    print("4. Exist ")
-
-    choice = input("Enter your choice: ")
-
-    if choice == "1":
-
-        name = input("Enter student name : ")
-
-        students.append(name)
-
-        print(name,"added successfully . ")
-
-    elif choice == "2":
-        if len(students)  == 0:
-            print("No student found . ")
-            continue
-        print("\n Mark Attendance ")
-
-        for student in students :
-            status = input(f"{student} P/A : ").upper()
-
-            if status == "P" :
-                attendances[student] = "Present"
-            elif status ==  "A":
-                attendances[student] = "Absent"
-                
-            else:
-                print("Invalid input.Marked as Absent. ")
-                attendances[student] = "Absent"
-
-    elif choice == "3":
-        print("\n===Attendance List ===== ")
-
-        if len(attendances)== 0 :
-            print("No attendance recorded yet . ")
-        else:
-            for student,status in attendances.items():
-                print(student,"-",status)
-
-    elif choice == "4":
-        print("program Ended . ")
-
-        break 
-    else:
-        print("Invalid Choice. ")
-
-
-        
+print_table(num)
