f1f5bbaab2a4ef9f988e0098f4e5c53bd6617196
Hjibhakate <hjibhakte2004@gmail.com>
2026-06-21 20:36:47 +0530

    class practice

--- full_python.py
+++ full_python.py
@@ -1,11 +1,19 @@
-# step  3 
 class Person :
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    def __init__(self,name ,age):
+        self.name = name 
+        self.age = age 
+    def greet(self):
+        print(f"my name is {self.name} and my age is {self.age}")
 
-    def display(self):
-        print(f"Name: {self.name}, Age: {self.age}")
+    def new_age(self):
+        self.age += 1
+        print(f"my new age is {self.age}")
 
-p1 = Person("Harshu", 25 )
-p1.display()
+    def new_name(self,change_name):
+        self.change_name = change_name
+        print(f"my new change name is {self.change_name}")
+
+p1 = Person("harshad",22)
+p1.greet()
+p1.new_age()
+p1.new_name("hemu")
